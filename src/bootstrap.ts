77304c64eed87677c794f0d3a7c92d66e074dc65
Kyle Neumeier <kneumei@gmail.com>
2017-01-27 21:38:09 -0600

    Add bootstrap 4

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,8 @@
 import 'zone.js';
 import 'reflect-metadata';
+import 'bootstrap'
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }  from './app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);   
