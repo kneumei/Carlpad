e744f24b2548bd06fb961fec803594f6858b454f
Kyle Neumeier <kneumei@gmail.com>
2017-02-03 17:06:17 -0600

    gamepad service

--- src/carlpad-gamepad-service.ts
+++ src/carlpad-gamepad-service.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CarlpadGamepadService {
 
+    constructor(){
+        window.addEventListener('gamepadconnected', (e) =>{
+            console.log(e);
+        })
+        window.addEventListener('gamepaddisconnected', (e) =>{
+            console.log(e);
+        })
+    }
+
     get gamepadData(): string{
         return "0,0";
     }
